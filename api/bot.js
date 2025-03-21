38d8f49ffe5600ab598adbae2fd692db4f94b73d
BilliStrom <165456158+BilliStrom@users.noreply.github.com>
2025-03-21 04:13:28 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -50,8 +50,7 @@ bot.on('text', async (ctx) => {
   }
 
   try {
-    const response = await fetch(
-      'https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill',
+    const response = await fetch('https://api-inference.huggingface.co/models/gpt2'),
       {
         method: 'POST',
         headers: {
