54dd34a12c4435b4f30e0af3c5f78c3cc8963458
BilliStrom <165456158+BilliStrom@users.noreply.github.com>
2025-03-21 05:30:16 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -58,7 +58,7 @@ bot.on('text', async (ctx) => {
     const timeout = setTimeout(() => controller.abort(), 9000);
 
     const response = await fetch(
-      'https://api-inference.huggingface.co/models/facebook/blenderbot-400M-distill',
+      const API_URL = 'https://api-inference.huggingface.co/models/ai-forever/rugpt3small_based_on_gpt2',
       {
         method: 'POST',
         headers: {
