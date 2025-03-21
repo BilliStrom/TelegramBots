4571260404813378bf1b6dc9054aa6dc0b8b9722
BilliStrom <165456158+BilliStrom@users.noreply.github.com>
2025-03-21 05:25:31 +0300

    Update bot.js

--- api/bot.js
+++ api/bot.js
@@ -68,7 +68,7 @@ bot.on('text', async (ctx) => {
         body: JSON.stringify({
           inputs: ctx.message.text,
           parameters: {
-            max_length: 20,
+            max_length: 30,
             temperature: 0.7,
             repetition_penalty: 1.5,
             wait_for_model: true
